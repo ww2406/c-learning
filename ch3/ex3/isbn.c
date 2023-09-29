7cce73773cc3fb9eb3345c2cede012ed56f5e2ed
William Welch <wwelch@myob>
2023-09-29 00:40:42 -0700

    Ch 3 Ex 3 ISBN

---
+++ ch3/ex3/isbn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int gsi, group, publisher, item, check;
+
+	printf("Enter ISBN: ");
+	scanf("%d-%d-%d-%d-%d", &gsi, &group, &publisher, &item, &check);
+
+	printf("GS1 Prefix: %d\n", gsi);
+	printf("Group identifier: %d\n", group);
+	printf("Publisher code: %d\n", publisher);
+	printf("Item number: %d\n", item);
+	printf("Check digit: %d\n", check);
+
+	return 0;
+}
