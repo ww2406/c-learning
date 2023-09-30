f66f841872b55345cd13b14dd53c1cef14a5248d
William Welch <wwelch@myob>
2023-09-29 17:10:54 -0700

    Ch 5 Ex 3 change to pps and num shares

--- ch5/ex3/broker.c
+++ ch5/ex3/broker.c
@@ -2,23 +2,29 @@
 
 int main(void) 
 {
-	float commission, value;
+	float commission, price_per_share, trade_value;
+	int num_shares;
 
-	printf("Enter value of trade: ");
-	scanf("%f", &value);
+	printf("Enter number of shares: ");
+	scanf("%d", &num_shares);
 
-	if (value < 2500.00f) {
-		commission = 30.0f + 0.017f * value;
-	} else if (value < 6250.00f) { 
-		commission = 56.00f + 0.0066f * value;
-	} else if (value < 20000.00f) {
-		commission = 76.00f + 0.0034f * value;
-	} else if (value < 50000.00f) {
-		commission = 100.00f + 0.0022f * value;
-	} else if (value < 500000.00f) {
-		commission = 155.00f + 0.0011f * value;
+	printf("Enter price per share: $");
+	scanf("%f", &price_per_share);
+
+	trade_value = num_shares * price_per_share;
+
+	if (trade_value < 2500.00f) {
+		commission = 30.0f + 0.017f * trade_value;
+	} else if (trade_value < 6250.00f) { 
+		commission = 56.00f + 0.0066f * trade_value;
+	} else if (trade_value < 20000.00f) {
+		commission = 76.00f + 0.0034f * trade_value;
+	} else if (trade_value < 50000.00f) {
+		commission = 100.00f + 0.0022f * trade_value;
+	} else if (trade_value < 500000.00f) {
+		commission = 155.00f + 0.0011f * trade_value;
 	} else {
-		commission = 255.00f + 0.0009f * value;
+		commission = 255.00f + 0.0009f * trade_value;
 	}
 
 	if (commission < 39.00f) {
